2c293b9eca66bb3f48445ed790c30ef0b75bd0c9
Shreekar Waghela <shreekar.waghela@mountblue.io>
2024-05-05 23:53:07 +0530

    initial commit

--- app/services/dynamodb_service.py
+++ app/services/dynamodb_service.py
@@ -1,19 +1,18 @@
-# services/dynamodb_service.py
 import boto3
-from ..settings import settings
+from dotenv import load_dotenv
+import os
 
-def get_item(item_id: str):
-    client = boto3.client(
+# Load environment variables
+load_dotenv()
+
+def get_dynamodb_resource():
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    aws_region = os.getenv('AWS_REGION')
+
+    return boto3.resource(
         'dynamodb',
-        aws_access_key_id=settings.aws_access_key_id,
-        aws_secret_access_key=settings.aws_secret_access_key,
-        region_name=settings.aws_default_region,
-        endpoint_url=settings.dynamodb_endpoint
+        region_name=aws_region,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
     )
-    response = client.get_item(
-        TableName='YourTableName',
-        Key={
-            'ItemId': {'S': item_id}
-        }
-    )
-    return response['Item']
