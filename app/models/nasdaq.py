d2137dcf9cc629812cf5240ff88ae9450a936012
Charlie <imreadytoplay8@gmail.com>
2024-06-19 20:24:36 -0400

    WIP

--- app/models/nasdaq.py
+++ app/models/nasdaq.py
@@ -1,26 +1,40 @@
 import asyncio
 import logging
-from datetime import datetime
+from datetime import datetime, time
+import os
 import pytz
 import aioboto3
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key, Attr
 import dotenv
 
+dotenv.load_dotenv()
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+MAX_CONCURRENT_QUERIES = 10
+QUERY_LIMIT = 10000
 
 
 def calculateNanoSec(hour, min, sec, milisec):
     return (hour * 3600 + min * 60 + sec) * 1000 * 1000000 + milisec * 1000000
 
 
-async def get_nasdaq_data(date=None):
-    session = aioboto3.Session()
+async def get_nasdaq_data(date=None, symbol="AAPL"):
+    start_time = datetime.now()
+    logger.info(f"Start time: {start_time}")
+
+    session = aioboto3.Session(
+        aws_access_key_id=os.getenv("NASDQA_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("NASDQA_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("NASDQA_DEFAULT_REGION"),
+    )
     async with session.resource("dynamodb") as dynamodb:
         try:
             nasdaq_table = await dynamodb.Table("NASDAQ2")
             await nasdaq_table.load()
+            logger.info(f"Loaded table at {datetime.now()}")
         except ClientError as e:
             if e.response["Error"]["Code"] == "ResourceNotFoundException":
                 logger.error("The table NASDAQ2 or index fetch_index does not exist.")
@@ -28,32 +42,38 @@ async def get_nasdaq_data(date=None):
             else:
                 raise e
 
-        utc_datetime = datetime.utcnow()
-        desired_timezone = pytz.timezone("America/New_York")
-        localized_datetime = utc_datetime.replace(tzinfo=pytz.utc).astimezone(
-            desired_timezone
-        )
-        midnight_time = datetime.combine(localized_datetime, datetime.min.time())
+        if date:
+            if isinstance(date, str):
+                date_obj = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+            elif isinstance(date, datetime):
+                date_obj = date
+            else:
+                raise ValueError("date must be a string or datetime object")
+        else:
+            utc_datetime = datetime.utcnow()
+            desired_timezone = pytz.timezone("America/New_York")
+            localized_datetime = utc_datetime.replace(tzinfo=pytz.utc).astimezone(
+                desired_timezone
+            )
+            date_obj = datetime.combine(localized_datetime, time.min)
 
         result = {
             "headers": ["trackingID", "date", "msgType", "symbol", "price"],
             "data": [],
         }
 
-        keyExpression = Key("date").eq(
-            date if date else midnight_time.strftime("%Y-%m-%d")
-        )
-        filterExpression = Attr("trackingID").gte(0)
+        keyExpression = Key("date").eq(date_obj.strftime("%Y-%m-%d"))
+        filterExpression = Attr("trackingID").gte(0) & Attr("symbol").eq(symbol)
 
         logger.info(
-            f"Querying NASDAQ data for date: {date if date else midnight_time.strftime('%Y-%m-%d')}"
+            f"Querying NASDAQ data for date: {date_obj.strftime('%Y-%m-%d %H:%M:%S')} and symbol: {symbol} at {datetime.now()}"
         )
 
         async def query_table(exclusive_start_key=None):
             query_args = {
                 "IndexName": "fetch_index",
                 "KeyConditionExpression": keyExpression,
-                "Limit": 100000,
+                "Limit": QUERY_LIMIT,
                 "FilterExpression": filterExpression,
             }
             if exclusive_start_key:
@@ -61,6 +81,9 @@ async def get_nasdaq_data(date=None):
 
             try:
                 response = await nasdaq_table.query(**query_args)
+                logger.info(
+                    f"Queried {len(response['Items'])} items at {datetime.now()}"
+                )
                 return response
             except ClientError as e:
                 if e.response["Error"]["Code"] == "ResourceNotFoundException":
@@ -71,13 +94,8 @@ async def get_nasdaq_data(date=None):
                 else:
                     raise e
 
-        response = await query_table()
-        if response is None:
-            return result
-
-        logger.info(f"Initial query returned {len(response['Items'])} items.")
-
         async def process_items(items):
+            logger.info(f"Processing {len(items)} items at {datetime.now()}")
             for item in items:
                 try:
                     result["data"].append(
@@ -85,48 +103,42 @@ async def get_nasdaq_data(date=None):
                             int(item["trackingID"]),
                             item["date"],
                             item["msgType"],
-                            item["symbol"] if "symbol" in item else "",
-                            int(item["price"]) if "price" in item else -1,
+                            item.get("symbol", ""),
+                            int(item.get("price", -1)),
                         ]
                     )
                 except Exception as e:
                     logger.error(f"Error occurs when saving data: {e}")
 
-        await process_items(response["Items"])
+        async def query_and_process(exclusive_start_key=None):
+            response = await query_table(exclusive_start_key)
+            if response is not None:
+                await process_items(response["Items"])
+                return response.get("LastEvaluatedKey", None)
+            return None
 
-        cnt = len(response["Items"])
-        tasks = []
+        semaphore = asyncio.Semaphore(MAX_CONCURRENT_QUERIES)
+        exclusive_start_key = await query_and_process()
 
-        while "LastEvaluatedKey" in response:
-            response = await query_table(response["LastEvaluatedKey"])
-            if response is None:
-                break
-            cnt += len(response["Items"])
-            logger.info(
-                f"Query returned {len(response['Items'])} items, total count so far: {cnt}"
-            )
-            tasks.append(process_items(response["Items"]))
+        async def query_worker():
+            nonlocal exclusive_start_key
+            while exclusive_start_key:
+                async with semaphore:
+                    exclusive_start_key = await query_and_process(exclusive_start_key)
 
-        if tasks:
-            await asyncio.gather(*tasks)
+        workers = [query_worker() for _ in range(MAX_CONCURRENT_QUERIES)]
+        await asyncio.gather(*workers)
 
-        logger.info(f"Total items retrieved: {cnt}")
+        end_time = datetime.now()
+        logger.info(f"End time: {end_time}")
+        logger.info(f"Total time taken: {end_time - start_time}")
+        logger.info(f"Total items retrieved: {len(result['data'])}")
+
         return result
 
 
-# if __name__ == "__main__":
-#     date_to_query = "2024-06-18"
-#     result = asyncio.run(get_nasdaq_data(date_to_query))
-#     print(result)
-
-
-async def list_dynamodb_tables():
-    session = aioboto3.Session()
-    async with session.client("dynamodb") as dynamodb_client:
-        response = await dynamodb_client.list_tables()
-        return response.get("TableNames", [])
-
-
 if __name__ == "__main__":
-    tables = asyncio.run(list_dynamodb_tables())
-    print("Tables in DynamoDB:", tables)
+    date_to_query = "2024-06-18 14:30:00"
+    symbol_to_query = "AAPL"
+    result = asyncio.run(get_nasdaq_data(date_to_query, symbol_to_query))
+    print(result)
